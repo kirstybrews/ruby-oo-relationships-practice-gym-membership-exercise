8c46c624e8c5c1a2fa6ee70b9fa05f2a80459c5f
github username <github email address>
2020-11-24 09:58:37 -0600

    Submitted by Kirsty B.

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,46 @@
 class Lifter
   attr_reader :name, :lift_total
 
+  @@all = []
+
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.average_lift_total
+    lift_totals = self.all.map do | lifter |
+      lifter.lift_total
+    end
+    average = lift_totals.sum.to_f / lift_totals.length.to_f
+  end
+
+  def gym_sign_up(gym, cost)
+    Membership.new(cost, self, gym)
+  end
+
+  def memberships
+    Membership.all.select do | membership |
+      membership.lifter == self
+    end
+  end
+
+  def gyms 
+    self.memberships.map do | membership |
+      membership.gym
+    end
+  end
+
+  def total_cost
+    prices = self.memberships.map do | membership |
+      membership.cost
+    end
+    prices.sum
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,7 +3,13 @@
 require_relative '../config/environment.rb'
 
 # test code goes here
-
+kirsty = Lifter.new("Kirsty",100)
+briana = Lifter.new("Briana", 100)
+crux = Gym.new("Crux Climbing Center")
+fit = Gym.new("24 Hour Fitness")
+memb1 = Membership.new(70, kirsty, crux)
+memb2 = Membership.new(20, kirsty, fit)
+memb3 = Membership.new(20, briana, fit)
 binding.pry
 
 puts "Gains!"
